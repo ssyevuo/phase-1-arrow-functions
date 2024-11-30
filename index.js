c1e9a5a033bff1b9c3dec123abb9d2c50ad9687a
ssyevuo <shakirasyevuo@gmail.com>
2024-11-30 13:25:58 +0300

    Completed lab

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide(x, y) {
+    return x/y;
+}
+console.log(divide(2000, 100))
+
+const square = n => {
+    return n * n;
+}
+console.log(square(4));
+
+const add = (x, y) => {
+    return x + y;
+}
+console.log(add(5, 6));
